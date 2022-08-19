c75108cfed42b3824ccc9467d638c41c8b2cae71
Andre Bakiewicz <bakiewicz@gmail.com>
2022-08-19 17:53:04 +0100

    watch list getting shape

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "lists#index"
-  resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create,]
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
+    resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: :destroy
 end
